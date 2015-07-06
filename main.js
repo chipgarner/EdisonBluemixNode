27da755f3f016bb1c48be0734b2b1f4a6e6a7a1f
cgarner <chip@garnertotalenergy.com>
2015-07-06 20:21:58 +0800

    Initial

---
+++ main.js
@@ -0,0 +1,48 @@
+/*jslint node:true, vars:true, bitwise:true, unparam:true */
+/*jshint unused:true */
+
+/*
+A simple node.js application intended to read data from Analog pins on the Intel based development boards such as the Intel(R) Galileo and Edison with Arduino breakout board.
+
+MRAA - Low Level Skeleton Library for Communication on GNU/Linux platforms
+Library in C/C++ to interface with Galileo & other Intel platforms, in a structured and sane API with port nanmes/numbering that match boards & with bindings to javascript & python.
+
+Steps for installing MRAA & UPM Library on Intel IoT Platform with IoTDevKit Linux* image
+Using a ssh client: 
+1. echo "src maa-upm http://iotdk.intel.com/repos/1.1/intelgalactic" > /etc/opkg/intel-iotdk.conf
+2. opkg update
+3. opkg upgrade
+
+Article: https://software.intel.com/en-us/html5/articles/intel-xdk-iot-edition-nodejs-templates
+*/
+
+
+
+var mraa = require('mraa'); //require mraa
+console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
+
+var analogPin0 = new mraa.Aio(0); //setup access analog input Analog pin #0 (A0)
+var analogValue = analogPin0.read(); //read the value of the analog pin
+console.log(analogValue); //write the value of the analog pin to the console
+
+var mqtt    = require('mqtt');
+var client  = mqtt.connect('mqtt://quickstart.messaging.internetofthings.ibmcloud.com:1883', { clientId: 'd:quickstart:iotquick-edison:784b87a801e9' });
+
+client.on('connect', function () {
+ // client.subscribe('presence');
+    var index = 0;
+    while (index < 10)
+    {
+        setInterval(function(){
+        client.publish('iot-2/evt/status/fmt/json', '{"d":{"Volts":1.3}}');
+        index ++;
+        }, 2000);
+        
+    }
+});
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString());
+  client.end();
+});
