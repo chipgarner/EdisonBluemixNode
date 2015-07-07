c60fa1fdd7aed3de9dbe5a130ecb254d0c8aa238
cgarner <chip@garnertotalenergy.com>
2015-07-07 17:57:45 +0800

    Cleaned up code and comments

--- main.js
+++ main.js
@@ -3,33 +3,35 @@
 
 /*
 Node.js application for connecting the Intel Edison Arduino to IBM Bluemix.
-
 Sends data from an analog sensor on analog pin zero.
 */
 
-//Uses mqtt.js, see package.json. More info at: https://www.npmjs.com/package/mqtt
+//Uses mqtt.js, see package.json. More info at: 
+//https://www.npmjs.com/package/mqtt
 var mqtt    = require('mqtt');
 
-var protocol = 'mqtt';
-var broker = 'quickstart.messaging.internetofthings.ibmcloud.com';// 'localhost' for local testing
-var port = 1883;
+var PROTOCOL = 'mqtt';
+var BROKER = 'quickstart.messaging.internetofthings.ibmcloud.com';// 'localhost' for local testing
+var PORT = 1883;
 
-var url = protocol + '://' + broker;
-url += ':' + port; //url is e.g. 'mqtt://quickstart.messaging.internetofthings.ibmcloud.com:1883'
+//Create the url string
+var URL = PROTOCOL + '://' + BROKER;
+URL += ':' + PORT; 
+//URL is e.g. 'mqtt://quickstart.messaging.internetofthings.ibmcloud.com:1883'
 
-var mac = '784b87a801e9';
-var clientid = 'd:quickstart:iotquick-edison:' + mac;
-var topic = 'iot-2/evt/status/fmt/json';
+var MAC = '784b87a801e9';
+var CLIENTID= 'd:quickstart:iotquick-edison:' + MAC;
+var TOPIC = 'iot-2/evt/status/fmt/json';
 
-var client  = mqtt.connect(url, { clientId: clientid });
+var client  = mqtt.connect(URL, { clientId: CLIENTID });
 
 client.on('connect', function () {
-    // Uncomment for local testing:
-    //client.subscribe(topic);
+  // Uncomment for local testing:
+  //client.subscribe(TOPIC);
     
-    setInterval(function(){
-        client.publish(topic, '{"d":{"Volts":' + analogVolts() + '}}');
-    }, 2000);//Keeps publishing every 2000 milliseconds.
+  setInterval(function(){
+    client.publish(TOPIC, '{"d":{"Volts":' + analogVolts() + '}}');//Payload is JSON
+  }, 2000);//Keeps publishing every 2000 milliseconds.
 });
 
 //Un-comment for local testing
@@ -37,13 +39,15 @@ client.on('connect', function () {
 //  console.log(message.toString());
 //});
 
-//Connect to an analog sensor on pin 0.
-//Uses mraa included with Edison image
+//Connect to an analog sensor on Edison Arduino pin A0.
+//Uses mraa included with Edison image.  More info at: 
+//http://iotdk.intel.com/docs/master/mraa/index.html
+//Edison Arduino returns drifting values if you have no sensor; you can see
+//the "data" on Bluemix if you have no sensor connected on pin A0.
 var mraa = require('mraa'); 
-console.log('MRAA Version: ' + mraa.getVersion());
 var pin0 = new mraa.Aio(0);
 var analogVolts = function() {
-    var counts = pin0.read();
-    var volts = counts * 4.95 / 1023;
-    return parseFloat(volts).toFixed(4);
+  var counts = pin0.read();
+  var volts = counts * 4.95 / 1023;
+  return parseFloat(volts).toFixed(4);
 };
